24bb2585e1052616f64866eff1ad1a7bf57b40de
thomas.liao256 <thomas.liao256@gmail.com>
2017-12-20 03:00:53 +0000

    t

--- tpcw/servlets/TPCW_home_interaction.java
+++ tpcw/servlets/TPCW_home_interaction.java
@@ -1,56 +1,3 @@
-/* 
- * TPCW_home_interaction.java - Servlet Class implements home interaction.
- *
- ************************************************************************
- *
- * This is part of the the Java TPC-W distribution,
- * written by Harold Cain, Tim Heil, Milo Martin, Eric Weglarz, and Todd
- * Bezenek.  University of Wisconsin - Madison, Computer Sciences
- * Dept. and Dept. of Electrical and Computer Engineering, as a part of
- * Prof. Mikko Lipasti's Fall 1999 ECE 902 course.
- *
- * Copyright (C) 1999, 2000 by Harold Cain, Timothy Heil, Milo Martin, 
- *                             Eric Weglarz, Todd Bezenek.
- *
- * This source code is distributed "as is" in the hope that it will be
- * useful.  It comes with no warranty, and no author or distributor
- * accepts any responsibility for the consequences of its use.
- *
- * Everyone is granted permission to copy, modify and redistribute
- * this code under the following conditions:
- *
- * This code is distributed for non-commercial use only.
- * Please contact the maintainer for restrictions applying to 
- * commercial use of these tools.
- *
- * Permission is granted to anyone to make or distribute copies
- * of this code, either as received or modified, in any
- * medium, provided that all copyright notices, permission and
- * nonwarranty notices are preserved, and that the distributor
- * grants the recipient permission for further redistribution as
- * permitted by this document.
- *
- * Permission is granted to distribute this code in compiled
- * or executable form under the same conditions that apply for
- * source code, provided that either:
- *
- * A. it is accompanied by the corresponding machine-readable
- *    source code,
- * B. it is accompanied by a written offer, with no time limit,
- *    to give anyone a machine-readable copy of the corresponding
- *    source code in return for reimbursement of the cost of
- *    distribution.  This written offer must permit verbatim
- *    duplication by anyone, or
- * C. it is distributed by someone who received only the
- *    executable form, and is accompanied by a copy of the
- *    written offer of source code that they received concurrently.
- *
- * In other words, you are welcome to use, share and improve this codes.
- * You are forbidden to forbid anyone else to use, share and improve what
- * you give them.
- *
- ************************************************************************/
-
 import java.io.*;
 import java.util.*;
 import javax.servlet.*;
@@ -69,25 +16,16 @@ public class TPCW_home_interaction extends HttpServlet {
       column2.addElement(new String("Dell"));
       column1.addElement(new String("Acer"));//2
       column2.addElement(new String("Asus"));
-      column1.addElement(new String("BUSINESS"));//3
-      column2.addElement(new String("POLITICS"));
-      column1.addElement(new String("CHILDREN"));//4
-      column2.addElement(new String("REFERENCE"));
-      column1.addElement(new String("COMPUTERS"));//5
-      column2.addElement(new String("RELIGION"));
-      column1.addElement(new String("COOKING"));//6
-      column2.addElement(new String("ROMANCE"));
-      column1.addElement(new String("HEALTH"));//7
-      column2.addElement(new String("SELF-HELP"));
-      column1.addElement(new String("HISTORY"));//8
-      column2.addElement(new String("SCIENCE-NATURE"));
-      column1.addElement(new String("HOME"));//9
-      column2.addElement(new String("SCIENCE-FICTION"));
-      column1.addElement(new String("HUMOR"));//10
-      column2.addElement(new String("SPORTS"));
-      column1.addElement(new String("LITERATURE"));//11
-      column2.addElement(new String("MYSTERY"));
-
+      column1.addElement(new String("Apple"));//3
+      column2.addElement(new String("Sansung"));
+      column1.addElement(new String("Alienware"));//4
+      column2.addElement(new String("Lenovo"));
+      column1.addElement(new String("Microsoft"));//5
+      column2.addElement(new String("MSI"));
+      column1.addElement(new String("Sony"));//6
+      column2.addElement(new String("Huawei"));
+      column1.addElement(new String("Compaq"));//7
+      column2.addElement(new String("Joso"));
       /*SERVLET SETUP*/
       HttpSession session = req.getSession(false);
       if(session == null)
